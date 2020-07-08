e6708cb61326914907031a4c4c6bd757524484d4
Diego-96 <diegopuentes96@gmail.com>
2020-07-08 10:18:22 -0500

    Se agrega logica para calcular el promedio de cinco notas

--- Promedio/Program.cs
+++ Promedio/Program.cs
@@ -6,7 +6,29 @@ namespace Promedio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int notaUno = Convert.ToInt32(Console.ReadLine());
+            int notaDos = Convert.ToInt32(Console.ReadLine());
+            int notaTres = Convert.ToInt32(Console.ReadLine());
+            int notaCuatro = Convert.ToInt32(Console.ReadLine());
+            int notaCinco = Convert.ToInt32(Console.ReadLine());
+
+            int resultadoSuma = notaUno + notaDos + notaTres + notaCuatro + notaCinco;
+            int resultadoDivision = resultadoSuma / 5;
+
+            if(resultadoDivision == 5)
+            {
+                Console.WriteLine("Excelente");
+            }
+
+            else if(resultadoDivision > 2.9 && resultadoDivision < 5)
+            {
+                Console.WriteLine("Aprobe");
+            }
+
+            else
+            {
+                Console.WriteLine("Reprobe");
+            }
         }
     }
 }
