fea1d8d9d38738ff384343a37afa3c37653abc34
Diego-96 <diegopuentes96@gmail.com>
2020-07-24 19:46:52 -0500

    Registro con array y for

---
+++ Registro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Registro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Digite un número:");
+            int cantidaNumeros = Convert.ToInt32(Console.ReadLine());
+            int[] numeros = new int[cantidaNumeros]; 
+            int numeroMayor = numeros[0];         
+            //declarara variable para guardar numero mayor
+
+            for (int i = 0; i < cantidadNumeros ; i++)
+            {
+                if()
+                {
+
+                } 
+                else
+                {
+                    
+                }
+            }
+            //recorrer el arreglo  con otro for y hacer un if para validar el numero mayor
+        }
+    }
+}
