b6ae9e46dd76b7c813dfe179478dd51f75d51913
Diego-96 <diegopuentes96@gmail.com>
2020-08-13 20:56:29 -0500

    Se agregan nuevos cambios en el Array

--- Tienda/Mercancia.cs
+++ Tienda/Mercancia.cs
@@ -2,28 +2,22 @@ using System;
 
 namespace Tienda
 {
-        public class Mercancia
-        {
-                Producto[] productos = new Producto[5];
-                productos[0] = new Producto { Nombre = "Camisa", Precio = 100000};
-        }
+    public class Mercancia
+    {
+        string[] productos = new string[5] {"Camisa", "Pantalon", "Medias", "Chaqueta", "Guantes"};
+    }
 
-        class Producto
-        {
-            public string Nombre { get; set; }
-            public int Precio { get; set; }
-            public bool Verificado { get; set; }
-        }
+    class Productos
+    {
+        public string Nombre { get; set;}
+        public int Precio { get; set; }
+        public bool Verificado { get; set; }
+    }
 
-        class Inventario
-        {
-            public int Categoria { get; set;}
-            public int CantidadDeUnidades { get; set; }
+    class Inventario
+    {
+        public int categoria { get; set; }
+        public int catidadUnidades { get; set; }
+    }
 
-            public void AgregarProducto( Producto productos)
-            {
-                var baseDeDatos = new List<Producto>();
-                baseDeDatos.Add(productos);
-            }
-        }
 }
