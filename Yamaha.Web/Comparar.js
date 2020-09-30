54daf8cf2751b5ed42db911d757d7f7b258f56d5
sergio.gil <sergio.gil@SG-558>
2020-09-30 17:04:14 -0500

    se ajusta función que cambia la imagen después de seleccionar una moto para que tome la imagen del localStorage

--- Yamaha.Web/Comparar.js
+++ Yamaha.Web/Comparar.js
@@ -1,42 +1,34 @@
+var motos = [];
 function CambiarImagen() {
-  var imagenDinamica = document.getElementById("imagenDinamica");
-  var listadoMotos = document.getElementById("listaMotos");
-  var motoSeleccionada = listadoMotos.value;
-
-  if (motoSeleccionada == 1) {
-    imagenDinamica.src = "./img/cabecera_ninja_zx10r_2011.jpg";
-  } else if (motoSeleccionada == 2) {
-    imagenDinamica.src = "./img/HondaCBR1000RR.jpg";
-  } else if (motoSeleccionada == 3) {
-    imagenDinamica.src = "./img/ktm-super-duke-r-2017.jpg";
-  } else {
-    imagenDinamica.src =
-      "https://us.123rf.com/450wm/urfandadashov/urfandadashov1809/urfandadashov180901275/109135379-foto-no-disponible-icono-del-vector-aislado-en-la-ilustraci%C3%B3n-transparente-transparente-concepto-de-esp.jpg?ver=6";
-  }
+	var imagenDinamica = document.getElementById("imagenDinamica");
+	var listadoMotos = document.getElementById("listaMotos");
+	var idMotoSeleccionada = listadoMotos.value;
+	var posicion = idMotoSeleccionada - 1;
+	imagenDinamica.src = motos[posicion].rutaImagen;
 }
 
 function MostrarEspecificaciones() {
-  var especificaciones = document.getElementById("especificaciones");
-  especificaciones.classList.remove("especificaciones-texto");
+	var especificaciones = document.getElementById("especificaciones");
+	especificaciones.classList.remove("especificaciones-texto");
 }
 
 function agregarMoto() {
-  window.location = "./NuevaMoto.html";
+	window.location = "./NuevaMoto.html";
 }
 
 function ListarMotos() {
-  var listaMotos = document.getElementById("listaMotos");
-  var motos = ConsultarMotos();
+	var listaMotos = document.getElementById("listaMotos");
+	ConsultarMotos();
 
-  for (var i = 0; i < motos.length; i++) {
-    var option = document.createElement("option");
-    option.value = i + 1;
-    option.innerHTML = motos[i].nombre;
-    listaMotos.appendChild(option);
-  }
+	for (var i = 0; i < motos.length; i++) {
+		var option = document.createElement("option");
+		option.value = i + 1;
+		option.innerHTML = motos[i].nombre;
+		listaMotos.appendChild(option);
+	}
 }
 
 function ConsultarMotos() {
-  var motos = localStorage.getItem("ListaMotos") || "[]";
-  return JSON.parse(motos);
+	var motosStorage = localStorage.getItem("ListaMotos") || "[]";
+	motos = JSON.parse(motosStorage);
 }
